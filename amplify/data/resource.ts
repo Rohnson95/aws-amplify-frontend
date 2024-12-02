c3d0dfa924dbbb2b3bacbaae46cd8ec6831daf01
Robert Johnson <rohnson95@gmail.com>
2024-12-02 19:01:04 +0100

    issues with fetching

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -16,7 +16,7 @@ const schema = a.schema({
       timestamp: a.timestamp().required(),
       temperature: a.float(),
       humidity: a.float(),
-      owner: a.string().required(),
+      owner: a.string(),
     })
     .identifier(['device_id', 'timestamp'])
     .authorization((allow) => [allow.owner(), allow.publicApiKey()]),
@@ -28,7 +28,7 @@ const schema = a.schema({
       timestamp: a.timestamp().required(),
       temperature: a.float(),
       humidity: a.float(),
-      owner: a.string().required()
+      owner: a.string(),
     }
     )
     .returns(a.ref("telemetry"))
@@ -38,7 +38,7 @@ const schema = a.schema({
   devices: a
     .model({
       device_id: a.string().required(),
-      owner: a.string().required(),
+      owner: a.string(),
       status: a.string(),
     })
     .identifier(['device_id'])
